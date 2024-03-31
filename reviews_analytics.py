d5c0f096f74ff9da41ddead0fc5f18b732f5cff8
YuTing4906 <tina6532656@gmail.com>
2024-03-31 17:47:07 +0800

    add new filtering

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -2,9 +2,11 @@
 # 題目 : 寫程式碼來讀取留言檔
 #       讀取檔案的過程中, 印出len(data)才知道目前讀取的進度
 #       計算留言的平均長度
+#       篩選有幾筆留言長度小於100
+#       篩選有幾筆留言有提到good
 
 data = []
-count = 0                              # 用來計算目前讀了幾筆資料
+count = 0    # 用來計算目前讀了幾筆資料
 
 with open('reviews.txt', 'r') as f:
     for line in f:
@@ -24,4 +26,11 @@ for d in data:
     if len(d) < 100:
         new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
-print(new[0])
+# print(new[0])
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆留言提到good')
+# print(good[0])
