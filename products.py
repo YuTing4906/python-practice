90e18ce0a46cf5f83adc24dc0b7405f5238f50b9
YuTing4906 <tina6532656@gmail.com>
2024-04-01 16:18:23 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,14 +1,20 @@
 # 建立記帳程式專案(+二維清單)
 
+import os     # 作業系統模組
+
 # 讀取檔案
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')          
-        products.append([name, price])                 
-print(products)
+if os.path.isfile('products.csv'):    # 檢查檔案在不在
+    print('yeah! 找到檔案了!')
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+             if '商品,價格' in line:
+                  continue
+             name, price = line.strip().split(',')
+             products.append([name, price])                 
+    print(products)
+else:
+    print('找不到檔案.....')
 
 # 讓使用者輸入
 while True:
@@ -25,6 +31,6 @@ for p in products:
 
 # 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
-    f.write('商品,價格\n')                      
+    f.write('商品,價格\n')                       
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')
