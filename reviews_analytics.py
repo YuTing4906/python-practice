0fde92d961da339baa6fc9dbc7cd89f0b1a17b47
YuTing4906 <tina6532656@gmail.com>
2024-04-02 23:32:42 +0800

    Add word count

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,13 +1,7 @@
 # [留言分析程式]
-# 題目 : 寫程式碼來讀取留言檔
-#       讀取檔案的過程中, 印出len(data)才知道目前讀取的進度
-#       計算留言的平均長度
-#       篩選有幾筆留言長度小於100
-#       篩選有幾筆留言有提到good
 
-data = []
+data = []    # 用來存一百萬筆留言
 count = 0    # 用來計算目前讀了幾筆資料
-
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
@@ -16,21 +10,52 @@ with open('reviews.txt', 'r') as f:
             print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
 
-sum_len = 0              # 用來計算目前留言的總長度
+
+# 計算留言的平均長度
+sum_len = 0              
 for d in data:
     sum_len += len(d)
 print('留言的平均長度為: ', sum_len / len(data))
 
+
+# 篩選有幾筆留言長度小於100
 new = []
 for d in data:
     if len(d) < 100:
         new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
-# print(new[0])
 
+
+# 篩選有幾筆留言有提到good
 good = []
 for d in data:
     if 'good' in d:
         good.append(d)
 print('一共有', len(good), '筆留言提到good')
-# print(good[0])
+
+
+# 文字計數
+wc = {}   # word_count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1    # 新增新的key進到word_count字典中
+
+print('---以下為出現超過一百萬次的字---')
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+print('字典內共有', len(wc), '個字')
+
+while True:
+    word = input('請問你想查什麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+        print('這個字沒有出現過喔!')
+print('感謝您使用本查詢功能!')
